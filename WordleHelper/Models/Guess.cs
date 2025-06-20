bc2329f02b217a055775aef2e19038d2e0d361a2
Dylan Logan <116318481+dLogan807@users.noreply.github.com>
2025-06-20 18:14:07 +1200

    add word data structures

---
+++ WordleHelper/Models/Guess.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordleHelper.Models;
+
+internal class Guess
+{
+    readonly Letter[] _letters = new Letter[5];
+
+    public void SetLetter(Letter letter, int position)
+    {
+        _letters[position] = letter;
+    }
+}
