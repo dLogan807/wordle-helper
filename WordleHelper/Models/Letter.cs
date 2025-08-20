fc5106815049a71fe144a4c0f75d55b6dd82a4e1
Dylan Logan <116318481+dLogan807@users.noreply.github.com>
2025-08-20 15:35:42 +1200

    add previous models

---
+++ WordleHelper/Models/Letter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordleHelper.Models;
+
+internal class Letter(char value, LetterCorrectness correctness)
+{
+    public char Value { get; set; } = value;
+
+    public LetterCorrectness Correctness { get; set; } = correctness;
+}
